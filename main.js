23185611931b9950b6b408d2248482e7a2e74f1a
Roy <roysdoan@pdx.edu>
2025-12-03 00:52:51 -0800

    planning on what should do overal, adding class, id and some test styling

---
+++ main.js
@@ -0,0 +1,11 @@
+//Next step: Create a 1440px to 1920px hero full profile just for visual about me
+// Navbar to 4 things
+// HOME+
+// resume
+// contact
+// about
+
+// JS should be handling mousehover Navbar
+// Smooth Transition
+// button click for social
+// a form for sending meesage for me and fake message?
